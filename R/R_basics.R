35ea1c04456e245b233c2e21c5a451fbf9083c57
atmathias <atmathias@gmail.com>
2021-04-08 17:07:20 +0300

    Added vectors

--- R/R_basics.R
+++ R/R_basics.R
@@ -3,3 +3,10 @@
 
 my_first_srting <- "helloworld"
 print(my_first_srting)
+
+# data structures
+# vectors:list of items of the same type.
+fruits <- c("banana", "mangoes", "orange")
+print(fruits)
+some_numbers <- c(1:10)
+print(some_numbers)
